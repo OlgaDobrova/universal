fb5ee478fb4474003927ed8eca0079d123468c74
OlgaDobrova <dobrova_o_g@mail.ru>
2021-03-06 22:34:34 +0300

    lesson-4.2

--- front-page.php
+++ front-page.php
@@ -4,44 +4,44 @@
     <div class="hero">
       <div class="left">
         <?php
-    //Объявляем глобальную переменную post
-    global $post;
+          //Объявляем глобальную переменную post
+          global $post;
 
-    $myposts = get_posts([ 
-      // вывести 1 последних постов
-    	'numberposts' => 1,
-    ]);
-    //проверка на наличие постов
-    //если есть
-    if( $myposts ){
-      //то запускаем цикл
-    	foreach( $myposts as $post ){
-    		setup_postdata( $post );
-    		?>
-    		<!-- Вывода постов, функции цикла: the_title() - заголовок записи и др.-->
-        <img src="<?php the_post_thumbnail_url() ?>" alt="" class="post-trumb">
-        <?php $autor_id = get_the_author_meta('ID');?>
-        <a href="<?php echo get_author_posts_url($autor_id);?>" class="autor">
-          <img src="<?php echo get_avatar_url($autor_id)?>" alt="" class="avatar">
-          <div class="autor-bio">
-            <span class="autor-name"><?php the_author();?></span>
-            <span class="autor-rank">Должность</span>
-          </div>
-        </a>
-        <div class="post-text">
-          <?php the_category( );?>
-          <h2 class="post-title"><?php the_title( );?></h2>
-          <a href="<?php echo get_the_permalink()?>" class="more">Читать далее</a>
-        </div>
-        <?php 
-    	      }
+          $myposts = get_posts([ 
+            // вывести 1 последних постов
+          	'numberposts' => 1,
+          ]);
+          //проверка на наличие постов
+          //если есть
+          if( $myposts ){
+            //то запускаем цикл
+          	foreach( $myposts as $post ){
+          		setup_postdata( $post );
+        ?>
+          		<!-- Вывода постов, функции цикла: the_title() - заголовок записи и др.-->
+              <img src="<?php the_post_thumbnail_url() ?>" alt="" class="post-trumb">
+              <?php $autor_id = get_the_author_meta('ID');?>
+              <a href="<?php echo get_author_posts_url($autor_id);?>" class="autor">
+                <img src="<?php echo get_avatar_url($autor_id)?>" alt="" class="avatar">
+                <div class="autor-bio">
+                  <span class="autor-name"><?php the_author();?></span>
+                  <span class="autor-rank">Должность</span>
+                </div>
+              </a>
+              <div class="post-text">
+                <?php the_category( );?>
+                <h2 class="post-title"><?php the_title( );?></h2>
+                <a href="<?php echo get_the_permalink()?>" class="more">Читать далее</a>
+              </div>
+              <?php 
+          	}
           } else {
-          	// Постов не найдено
-            ?><p>Постов не найдено</p><?php
-          }
-
-    wp_reset_postdata(); // Сбрасываем $post
-  ?>
+                	// Постов не найдено
+              ?><p>Постов не найдено</p><?php
+            }
+              
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
       </div>
       <!--/.left/ -->
       <div class="right">
@@ -62,29 +62,63 @@
               //то запускаем цикл
             	foreach( $myposts as $post ){
             		setup_postdata( $post );
-            		?>
-          <l1 class="post">
-            <?php the_category( );?>
-            <a class="post-permalink" href="<?php echo get_the_permalink()?>">
-                <h4 class="post-title"><?php the_title();?></h4>
-            </a>
-          </l1>
-          <?php 
-    	      }
-          } else {
+                ?>
+                <li class="post">
+                <?php the_category( );?>
+                <a class="post-permalink" href="<?php echo get_the_permalink()?>">
+                  <h4 class="post-title"><?php the_title();?></h4>
+                </a>
+                </li>
+                <?php 
+    	        }
+            } else {
           	// Постов не найдено
             ?><p>Постов не найдено</p><?php
-          }
+            }
 
-    wp_reset_postdata(); // Сбрасываем $post
-  ?>
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
         </ul>
       <!--/.right/ -->
     </div>
     <!--/.hero/ -->
-
-   		
-
   </div>
   <!--/.container/ -->
 </main>
+<div class="container">
+  <ul class="article-list">
+    <?php
+      //Объявляем глобальную переменную post
+      global $post;
+
+      $myposts = get_posts([ 
+      	'numberposts' => 4,
+      ]);
+      //проверка на наличие постов
+      //если есть
+      if( $myposts ){
+        //то запускаем цикл
+      	foreach( $myposts as $post ){
+      		setup_postdata( $post );
+    ?>
+    <li class="article-item">
+      <a class="article-permalink" href="<?php echo get_the_permalink()?>">
+        <h4 class="article-title">
+          <?php the_title();?>
+        </h4>
+      </a>
+      <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url(null,'thumbnail')?>" alt="" class="">
+    </li>
+    <?php 
+      	}
+      } else {
+      	// Постов не найдено
+        ?>
+        <p>Постов не найдено</p>
+        <?php
+      }
+      wp_reset_postdata(); // Сбрасываем $post
+    ?>
+  </ul>
+</div>
+<!--/.container/ --> 
