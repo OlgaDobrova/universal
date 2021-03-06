4660a9d5a0a38babd44db9ca4add1de681cd7f32
OlgaDobrova <dobrova_o_g@mail.ru>
2021-03-06 11:45:53 +0300

    lesson-4

--- front-page.php
+++ front-page.php
@@ -1,50 +1,90 @@
 <?php get_header( );?>
-<main>
+<main class="front-page-header">
   <div class='container'>
     <div class="hero">
       <div class="left">
-        <img src="<?php echo get_template_directory_uri().'/assets/images/images.jpg'?>" alt="" class="post-trumb">
-        <a href="#" class="autor">
-          <img src="<?php echo get_template_directory_uri().'/assets/images/avatar.png'?>" alt="" class="avatar">
+        <?php
+    //Объявляем глобальную переменную post
+    global $post;
+
+    $myposts = get_posts([ 
+      // вывести 1 последних постов
+    	'numberposts' => 1,
+    ]);
+    //проверка на наличие постов
+    //если есть
+    if( $myposts ){
+      //то запускаем цикл
+    	foreach( $myposts as $post ){
+    		setup_postdata( $post );
+    		?>
+    		<!-- Вывода постов, функции цикла: the_title() - заголовок записи и др.-->
+        <img src="<?php the_post_thumbnail_url() ?>" alt="" class="post-trumb">
+        <?php $autor_id = get_the_author_meta('ID');?>
+        <a href="<?php echo get_author_posts_url($autor_id);?>" class="autor">
+          <img src="<?php echo get_avatar_url($autor_id)?>" alt="" class="avatar">
           <div class="autor-bio">
-            <span class="autor-name">Имя автора</span>
+            <span class="autor-name"><?php the_author();?></span>
             <span class="autor-rank">Должность</span>
           </div>
         </a>
         <div class="post-text">
-          <a href="#" class="category-name">Рубрики</a>
-          <h2 class="post-title">Название поста</h2>
-          <a href="#" class="more">Читать далее</a>
+          <?php the_category( );?>
+          <h2 class="post-title"><?php the_title( );?></h2>
+          <a href="<?php echo get_the_permalink()?>" class="more">Читать далее</a>
         </div>
+        <?php 
+    	      }
+          } else {
+          	// Постов не найдено
+            ?><p>Постов не найдено</p><?php
+          }
+
+    wp_reset_postdata(); // Сбрасываем $post
+  ?>
       </div>
       <!--/.left/ -->
       <div class="right">
         <h3 class="recommend">Рекомендуем</h3>
         <ul class="posts-list">
+          <?php
+            //Объявляем глобальную переменную post
+            global $post;
+
+            $myposts = get_posts([ 
+              // вывести 5 последних постов
+            	'numberposts' => 5,
+              'offset'  => 1,
+            ]);
+            //проверка на наличие постов
+            //если есть
+            if( $myposts ){
+              //то запускаем цикл
+            	foreach( $myposts as $post ){
+            		setup_postdata( $post );
+            		?>
           <l1 class="post">
-            <span class="category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
+            <?php the_category( );?>
+            <a class="post-permalink" href="<?php echo get_the_permalink()?>">
+                <h4 class="post-title"><?php the_title();?></h4>
+            </a>
           </l1>
-          <l1 class="post">
-            <span class="category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </l1>
-          <l1 class="post">
-            <span class="category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </l1>
-          <l1 class="post">
-            <span class="category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </l1>
-          <l1 class="post">
-            <span class="category-name">Категория</span>
-            <h4 class="post-title">Название поста в две строки</h4>
-          </l1>
+          <?php 
+    	      }
+          } else {
+          	// Постов не найдено
+            ?><p>Постов не найдено</p><?php
+          }
+
+    wp_reset_postdata(); // Сбрасываем $post
+  ?>
         </ul>
       <!--/.right/ -->
     </div>
     <!--/.hero/ -->
+
+   		
+
   </div>
   <!--/.container/ -->
 </main>
