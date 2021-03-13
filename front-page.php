ccfbcb9c19106cebc7a16c26899e63594ed1607f
OlgaDobrova <dobrova_o_g@mail.ru>
2021-03-13 20:52:16 +0300

    lesson-8-9

--- front-page.php
+++ front-page.php
@@ -95,6 +95,7 @@
 
       $myposts = get_posts([ 
       	'numberposts' => 4,
+        'category_not_in' => 24 , //tag_ID=24 - это рубрика расследования
         'category_name' => 'articles',
       ]);
       //проверка на наличие постов
@@ -124,15 +125,18 @@
     ?>
   </ul>
   <!--/.article-list/ --> 
+
   <!--article-grid - Сетка статей-->
-  <div class="main-grid">
-    <ul class="article-grid">
-      <?php		
+  <div class="main-grid">  <!--main-grid - Моя сетка из 7 статей-->
+    <ul class="article-grid"> <!--Тег <ul> устанавливает маркированный список-->
+                                 <!--<li>элемент маркированного списка</li>-->
+      <?php		                 
         global $post;
         //формируем запрос в базу данных    
         $query = new WP_Query( [
           //получаем 7 постов
         	'posts_per_page' => 7,
+          'category_not_in' => 24, //tag_ID=24 - это рубрика расследования, ее исключаем
         ] );
         
         //проверяем наличие постов
@@ -222,7 +226,7 @@
                           <!--иконку-->
                           <img src="<?php echo get_template_directory_uri().'/assets/images/comment-white.svg'?>" alt="icon : comment" class="comments-icon">
                           <!--кол-во-->
-                          <span class="comments-counter"><?php comments_number('0','1', '%') ?></ span>
+                          <span class="comments-counter"><?php comments_number('0','1', '%') ?></span>
                         </div>
                         <div class="likes">
                           <!--иконку для лайков-->
@@ -293,3 +297,103 @@
   <!--/.main-grid/ --> 
 </div>
 <!--/.container/ --> 
+
+
+<!--/открытие секции - расследование недели.../ --> 
+<?php		
+global $post;
+
+$query = new WP_Query( [
+	'posts_per_page' => 1,
+	'category_name'  => 'investigation',
+] );
+
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		?>
+		  <section class="investigation" style="background-image: linear-gradient(0deg, rgba(64, 48, 61, 0.45), rgba(64, 48, 61, 0.45)), url(benjamin-lambert-KxdO8elL5_c-unsplash.jpg), url(<?php echo get_the_post_thumbnail_url()?>)">
+        <div class="container">
+          <h2 class="investigation-title"><?php the_title();?></h2>
+          <a href="<?php echo get_the_permalink()?>" class="more">Читать статью</a>
+        </div>
+        <!--/.container/ -->
+      </section>
+      <!--/.section/ --> 
+		<?php 
+	}
+} else {
+	// Постов не найдено
+}
+
+wp_reset_postdata(); // Сбрасываем $post
+?>
+
+<div class="container">  <!--/контейнер будет состоять из дайджеста слева и сайдбара справа/ --> 
+  <!--/открытие левой секции контейнера - ДАЙДЖЕСТ - подборка статей.../ --> 
+  <div class="digest-wrapper">
+    <ul class="digest"> <!--маркированный список --> 
+      <?php
+        //Объявляем глобальную переменную post
+        global $post;
+
+        $myposts = get_posts([ 
+        	'numberposts' => 6,
+        ]);
+        //проверка на наличие постов
+        //если есть
+        if( $myposts ){
+          //то запускаем цикл
+        	foreach( $myposts as $post ){
+        		setup_postdata( $post );
+      ?>
+      <li class="digest-item"> <!--элемент списка -->
+        <a class="digest-item-permalink" href="<?php echo get_the_permalink()?>">
+          <img src="<?php echo get_the_post_thumbnail_url(null,  'homepage-thumb')?>" alt="" class="digest-item-permalink-thumb">
+          <!--всю информацию отделю от картинки-->
+          <div class="digest-item-permalink-info">
+            <!--выводим категорию-->
+            <span class="digest-item-permalink-info-category"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+            <!--выводим наименование статьи-->
+            <h4 class="digest-item-permalink-info-title"> 
+              <?php echo mb_strimwidth(get_the_title(),0,50,'...') ;?>
+            </h4>
+            <!--выводим отрывок статьи-->
+            <p class="digest-item-permalink-info-excerpt">
+              <?php echo mb_strimwidth(get_the_excerpt(),0,130,'...'); ?>
+            </p>
+            <div class="digest-item-permalink-info-footer"> <!--подвал каждого поста-->
+              <!--выводим дату статьи в формате день j,месяц F, год Y, напр. 1 мая 2021-->
+              <span class="digest-date"><?php the_time('j F');?></span>
+              <!--выводим в блоке инф о комментариях-->
+              <div class="comments digest-item-permalink-info-footer-comments">
+                <!--иконку-->
+                <img width="19" height="15" src="<?php echo get_template_directory_uri().'/assets/images/comment.svg' ?>"    alt="icon : comment" class="icon digest-item-permalink-info-footer-comments-icon">
+                <!--кол-во-->
+                <span class="digest-item-permalink-info-footer-comments-counter"><?php comments_number('0','1', '%') ?></span>
+              </div>
+              <div class="likes digest-item-permalink-info-footer-likes">
+                <!--иконку для лайков-->
+                <img width="19" height="15" src="<?php echo get_template_directory_uri().'/assets/images/heart-color.svg' ?>"   alt="icon: like" class="digest-item-permalink-info-footer-likes-icon">
+                <!--кол-во-->
+                <span class="digest-item-permalink-info-footer-likes-counter"><?php comments_number('0','1', '%') ?></span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </li>
+      <?php 
+        	}
+        } else {
+        	// Постов не найдено
+          ?>
+          <p>Постов не найдено</p>
+          <?php
+        }
+        wp_reset_postdata(); // Сбрасываем $post
+      ?>
+    </ul>
+  </div>
+  <!--/.digest-wrapper/ -->
+</div>
+<!--/.container/ -->
